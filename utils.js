ea5a8e4be8772b4fce485c128975f96ad752366a
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 18:07:43 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T15:01:03.935Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-09T15:07:42.603Z
+Commit: Patch API endpoint error handling
