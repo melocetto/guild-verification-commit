0eecdf3de2dde2105ae19d1f929e205cae8cde28
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 18:07:45 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T15:07:42.603Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T15:07:45.522Z
+Commit: Resolve timezone issues in date handling
