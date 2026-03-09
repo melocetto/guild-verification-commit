4fa74baed3418b33ef41d16b49ed2bb93effa385
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 18:01:04 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T15:00:59.673Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T15:01:03.935Z
+Commit: Add export functionality for reports
