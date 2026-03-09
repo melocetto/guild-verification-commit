009ccc39bd90681e266665b7643f6b76bcd4e8b2
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:56:20 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:56:06.387Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T14:56:19.951Z
+Commit: Resolve null pointer exception in user service
