675fc81629d142978cc358242f82e63855b8faeb
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 18:00:33 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T15:00:32.000Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T15:00:33.288Z
+Commit: Patch API endpoint error handling
