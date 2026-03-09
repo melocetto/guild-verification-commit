bcd7ab7dcbd76d4aadcd05bee7cf4650f190117f
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:58:02 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:57:57.679Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T14:58:01.800Z
+Commit: Patch security vulnerability in authentication
