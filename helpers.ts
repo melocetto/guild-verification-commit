af899b0457929f66b03aaec740d526efa2796cb8
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:59:30 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:59:22.866Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T14:59:30.159Z
+Commit: Patch security vulnerability in authentication
