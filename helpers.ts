48411b1877130d30cb5db4117772e4d10c0a9d40
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:57:54 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:56:22.979Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-09T14:57:54.312Z
+Commit: Resolve timezone issues in date handling
