8a781e50dc8a1db660750e84bfde1e3ed088d6e2
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:57:58 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:57:54.312Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T14:57:57.679Z
+Commit: Patch API endpoint error handling
