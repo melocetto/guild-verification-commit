394fba0f53de955996f0ebeaa4187fc67f6fc2ec
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:58:21 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:58:11.126Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-09T14:58:21.424Z
+Commit: Fix broken links in documentation
