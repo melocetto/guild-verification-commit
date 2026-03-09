5cc8a67199703e96ae7c79c1ea30126e46dc0607
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:55:09 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:54:44.396Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-09T14:55:08.700Z
+Commit: Fix memory leak in event handlers
