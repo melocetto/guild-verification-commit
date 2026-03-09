936725c3dbdcdf2924f528d4832ce0f880b74d6d
melocetto <98284527+melocetto@users.noreply.github.com>
2026-03-09 17:56:09 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T14:54:40.301Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T14:56:09.057Z
+Commit: Implement user profile customization
